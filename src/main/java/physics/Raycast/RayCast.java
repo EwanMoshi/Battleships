d430c0587da6c86d99a5b6130e9859b273f90559
EwanMoshi <ewanmoshi@live.com>
2015-07-25 00:37:57 +1200

    RayCast.java modification

--- src/main/java/physics/Raycast/RayCast.java
+++ src/main/java/physics/Raycast/RayCast.java
@@ -1,5 +1,23 @@
 package main.java.physics.Raycast;
 
+import org.lwjgl.util.vector.Vector3f;
+
 public class RayCast {
 
+	/**
+	 * This class does calculations with the 'currentRay' (from main.java.gfx.toolbox) on game world.
+	 * 
+	 * (A method that may be useful is inside BoundingSphere.java called contains(Vector3f Point)) 
+	 */
+	
+		
+	
+	/**
+	 * Check if the current ray is colliding with any real objects
+	 * @param ray - the ray we are checking
+	 * @return
+	 */
+	public static boolean isColliding(Vector3f ray) {
+		return false;
+	}
 }
